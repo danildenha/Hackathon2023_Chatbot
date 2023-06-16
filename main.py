d07c52ca3d157eca28ae65caa9f6abd7f4b81219
Mak5er <mmaksym.reva@gmail.com>
2023-06-16 22:46:00 +0300

    update new buttons

--- main.py
+++ main.py
@@ -88,13 +88,17 @@ async def start_taryf(message: types.Message):
 
     if user_language == "ua":
         start_keyboard_ua = types.InlineKeyboardMarkup()
-        start_keyboard_ua.add(types.InlineKeyboardButton(text="🔎Почати підбір", callback_data="age_survey"))
+        start_keyboard_ua.add(types.InlineKeyboardButton(text="🔎Підібрати тариф", callback_data="age_survey"),
+                              types.InlineKeyboardButton(text="🌐Всі тарифи",
+                                                         url="https://www.lifecell.ua/uk/mobilnij-zvyazok/taryfy/"))
         await bot.send_message(user_id, f"Привіт *{name}*! Я допоможу тобі знайти найкращий для тебе тариф Lifecell!",
                                reply_markup=start_keyboard_ua, parse_mode="Markdown")
 
     elif user_language == "en":
         start_keyboard_en = types.InlineKeyboardMarkup()
-        start_keyboard_en.add(types.InlineKeyboardButton(text="🔎Start selection", callback_data="age_survey"))
+        start_keyboard_en.add(types.InlineKeyboardButton(text="🔎Start selection", callback_data="age_survey"),
+                              types.InlineKeyboardButton(text="🌐All tariffs",
+                                                         url="https://www.lifecell.ua/en/mobile/tariffs/"))
         await bot.send_message(user_id, f"Hello *{name}*! I will help you to find the best Lifecell tariff!",
                                reply_markup=start_keyboard_en, parse_mode="Markdown")
 
@@ -156,6 +160,7 @@ Please select your age.""", reply_markup=age_keyboard, parse_mode="Markdown")
     # Додаємо поточну функцію до стеку
     stack.append(undersood_handler)
 
+
 @dp.callback_query_handler(lambda call: call.data == 'less_than_eighteen')
 async def less_than_eighteen(call: types.CallbackQuery):
     name = call.from_user.full_name
@@ -163,34 +168,39 @@ async def less_than_eighteen(call: types.CallbackQuery):
     user_language = get_user_language(user_id)
 
     if user_language == "ua":
-        school_life = types.InlineKeyboardButton(text="📲Підключити", url="https://www.lifecell.ua/uk/mobilnij-zvyazok/taryfy/shkilniy/")
+        school_life = types.InlineKeyboardButton(text="📲Підключити",
+                                                 url="https://www.lifecell.ua/uk/mobilnij-zvyazok/taryfy/shkilniy/")
         not_interest = types.InlineKeyboardButton(text="❌Не цікаво", callback_data="more_than_eighteen")
         back_button = types.InlineKeyboardButton(text="⬅ Назад", callback_data="back")
-
 
         school_life_keyboard = types.InlineKeyboardMarkup()
         school_life_keyboard.row(school_life)
         school_life_keyboard.row(not_interest)
         school_life_keyboard.row(back_button)
 
-        await call.message.edit_text(text=f"*{name}*, ми помітили, що вам менше 18-ти. Тому пропонуємо вам тариф '*Шкільний Лайф*', який зроблений спеціально для школярів.", parse_mode="Markdown", reply_markup=school_life_keyboard)
+        await call.message.edit_text(
+            text=f"*{name}*, ми помітили, що вам менше 18-ти. Тому пропонуємо вам тариф '*Шкільний Лайф*', який зроблений спеціально для школярів.",
+            parse_mode="Markdown", reply_markup=school_life_keyboard)
 
     if user_language == "en":
-            school_life = types.InlineKeyboardButton(text="📲Connect", url="https://www.lifecell.ua/uk/mobilnij-zvyazok/taryfy/shkilniy/")
-            not_interest = types.InlineKeyboardButton(text="❌Not interested", callback_data="more_than_eighteen")
-            back_button = types.InlineKeyboardButton(text="⬅ Back", callback_data="back")
+        school_life = types.InlineKeyboardButton(text="📲Connect",
+                                                 url="https://www.lifecell.ua/uk/mobilnij-zvyazok/taryfy/shkilniy/")
+        not_interest = types.InlineKeyboardButton(text="❌Not interested", callback_data="more_than_eighteen")
+        back_button = types.InlineKeyboardButton(text="⬅ Back", callback_data="back")
 
+        school_life_keyboard = types.InlineKeyboardMarkup()
+        school_life_keyboard.row(school_life)
+        school_life_keyboard.row(not_interest)
+        school_life_keyboard.row(back_button)
 
-            school_life_keyboard = types.InlineKeyboardMarkup()
-            school_life_keyboard.row(school_life)
-            school_life_keyboard.row(not_interest)
-            school_life_keyboard.row(back_button)
-
-            await call.message.edit_text(text=f"*{name}*, we noticed that you are under 18. That's why we offer you the '*School Life*' tariff, which is specially designed for schoolchildren.", parse_mode="Markdown", reply_markup=school_life_keyboard)
+        await call.message.edit_text(
+            text=f"*{name}*, we noticed that you are under 18. That's why we offer you the '*School Life*' tariff, which is specially designed for schoolchildren.",
+            parse_mode="Markdown", reply_markup=school_life_keyboard)
 
     stack.append(less_than_eighteen)
 
 
+# noinspection PyUnboundLocalVariable
 @dp.callback_query_handler(lambda call: call.data == 'more_than_eighteen')
 async def more_than_eighteen(call: types.CallbackQuery):
     name = call.from_user.full_name
@@ -198,34 +208,94 @@ async def more_than_eighteen(call: types.CallbackQuery):
     user_language = get_user_language(user_id)
 
     if user_language == "ua":
-        own_button = types.InlineKeyboardButton(text="🙋‍♂️Для себе", callback_data="back")
-        family_button = types.InlineKeyboardButton(text="👨‍👩‍👧‍👦Для сім'ї", callback_data="back")
-        for_gadget_button = types.InlineKeyboardButton(text="💻Для Ґаджета", callback_data="back")
+        own_button = types.InlineKeyboardButton(text="🙋‍♂️Для себе", callback_data="for_me")
+        family_button = types.InlineKeyboardButton(text="👨‍👩‍👧‍👦Для сім'ї", callback_data="for_family")
+        for_gadget_button = types.InlineKeyboardButton(text="💻Для Ґаджета", callback_data="for_gadget")
+        what_difference_button = types.InlineKeyboardButton(text="В чому різниця❓", callback_data="what_difference")
         back_button = types.InlineKeyboardButton(text="⬅ Назад", callback_data="back")
 
-        usage_select = types.InlineKeyboardMarkup()
-        usage_select.row(own_button)
-        usage_select.row(family_button, for_gadget_button )
-        usage_select.row(back_button)
+        usage_select_ua = types.InlineKeyboardMarkup()
+        usage_select_ua.row(own_button)
+        usage_select_ua.row(family_button, for_gadget_button)
+        usage_select_ua.row(what_difference_button)
+        usage_select_ua.row(back_button)
 
         await call.message.edit_text(
-            text=f"*{name}*, для якого типу використання вам потрібен тариф?", parse_mode="Markdown", reply_markup=usage_select)
+            text=f"*{name}*, для якого типу використання вам потрібен тариф?", parse_mode="Markdown",
+            reply_markup=usage_select_ua)
+
+    elif user_language == "en":
+        own_button = types.InlineKeyboardButton(text="🙋‍♂️For yourself", callback_data="for_me")
+        family_button = types.InlineKeyboardButton(text="👨‍👩‍👧‍👦For family", callback_data="for_family")
+        for_gadget_button = types.InlineKeyboardButton(text="💻For Gadget", callback_data="for_gadget")
+        what_difference_button = types.InlineKeyboardButton(text="What's the difference❓",
+                                                            callback_data="what_difference")
+        back_button = types.InlineKeyboardButton(text="⬅ Back", callback_data="back")
+
+        usage_select_en = types.InlineKeyboardMarkup()
+        usage_select_en.row(own_button)
+        usage_select_en.row(family_button, for_gadget_button)
+        usage_select_en.row(what_difference_button)
+        usage_select_en.row(back_button)
+
+        await call.message.edit_text(
+            text=f"*{name}*, for what type of use you need the tariff?", parse_mode="Markdown",
+            reply_markup=usage_select_en)
 
     stack.append(more_than_eighteen)
+
+
+@dp.callback_query_handler(lambda call: call.data == 'for_me')
+async def how_much_speak(call: types.CallbackQuery):
+    name = call.from_user.full_name
+    user_id = call.from_user.id
+    user_language = get_user_language(user_id)
+
+    if user_language == "ua":
+        sometimes_button = types.InlineKeyboardButton(text="Балакаю по необхідності", callback_data="call_sometimes")
+        like_long_calls_button = types.InlineKeyboardButton(text="Частенько можу заговоритись",
+                                                            callback_data="call_long_calls")
+        everytime_on_phone_button = types.InlineKeyboardButton(text="Жити не можу без довгих розмов по телефону",
+                                                               callback_data="call_everytime_on_phone")
+        back_button = types.InlineKeyboardButton(text="⬅ Назад", callback_data="back")
+
+        calls_keyboard_ua = types.InlineKeyboardMarkup()
+        calls_keyboard_ua.row(sometimes_button)
+        calls_keyboard_ua.row(like_long_calls_button)
+        calls_keyboard_ua.row(everytime_on_phone_button)
+        calls_keyboard_ua.row(back_button)
+
+        await call.message.edit_text(text=f"*{name}*, будьласка обери як часто ти спілкуєшся по телефону.",
+                                     parse_mode="Markdown", reply_markup=calls_keyboard_ua)
+
+    elif user_language == "en":
+        sometimes_button = types.InlineKeyboardButton(text="Балакаю по необхідності", callback_data="call_sometimes")
+        like_long_calls_button = types.InlineKeyboardButton(text="Частенько можу заговоритись",
+                                                            callback_data="call_long_calls")
+        everytime_on_phone_button = types.InlineKeyboardButton(text="Жити не можу без довгих розмов по телефону",
+                                                               callback_data="call_everytime_on_phone")
+        back_button = types.InlineKeyboardButton(text="⬅ Назад", callback_data="back")
+
+        calls_keyboard_en = types.InlineKeyboardMarkup()
+        calls_keyboard_en.row(sometimes_button)
+        calls_keyboard_en.row(like_long_calls_button)
+        calls_keyboard_en.row(everytime_on_phone_button)
+        calls_keyboard_en.row(back_button)
+
+        await call.message.edit_text(text=f"*{name}*, please select how often you talk by phone.",
+                                     parse_mode="Markdown", reply_markup=calls_keyboard_en)
+
 
 
 @dp.callback_query_handler(lambda call: call.data == 'back')
 async def back_handler(call: types.CallbackQuery):
     if len(stack) > 1:
-        # Видаляємо поточну функцію зі стеку
         stack.pop()
-        # Отримуємо попередню функцію зі стеку
         previous_function = stack[-1]
-        # Викликаємо попередню функцію
         await previous_function(call)
     else:
-        # Якщо стек порожній, виконуємо дії для випадку, коли немає попередньої функції
         await call.message.edit_reply_markup(reply_markup=None)
+
 
 @dp.callback_query_handler()
 async def language_callback(call: types.CallbackQuery):
